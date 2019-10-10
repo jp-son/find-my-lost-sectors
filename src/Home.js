da9a84b1fdd315b8568e1ce7562c079dbc01a67c
jp-son <philson201@gmail.com>
2019-10-10 02:19:08 -0400

    Created data file for lost sector info

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {lsdata} from './lostsectordata.js'
+
+class Home extends React.Component {
+
+
+
+
+
+	render() {
+		console.log(lsdata.io[4137615299]);
+		return (
+			<div> Hello </div>
+		)
+	}	
+}
+
+export default Home;
