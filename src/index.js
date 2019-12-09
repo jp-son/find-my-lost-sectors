70be82aabb696adc81df80bba7cfa04a6b76a88b
Jaejoon Son <philson201@gmail.com>
2019-12-08 22:40:47 -0500

    Fixed more merge issues

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-<<<<<<< HEAD
 //import 'bootstrap/dist/css/bootstrap.min.css';
-=======
->>>>>>> 0ebd00576ab67637e2bfc3f05cb86f4d4a2bb11c
 
 ReactDOM.render((
 	<BrowserRouter>
